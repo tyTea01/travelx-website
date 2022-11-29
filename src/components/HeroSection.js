07f39f8362b4241bb2aa1bb58d940dc679d9b2a4
tyTea01 <tysonh288@gmail.com>
2022-11-28 19:35:02 -0800

    third update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,28 @@
 
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
+import { Button, TextField } from '@mui/material';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-3.mp4' autoPlay loop muted />
       <h1>Enter your SSN</h1>
+      <div className='ssn-submit'>
+        <header>
+          <TextField
+            label="Social Security"
+            helperText="Please enter your SSN"
+            id="outlined-size-small"
+            size='small'
+            placeholder='xxx-xx-xxxx'
+          />
+          <Button type="submit" variant="contained" style={{margin: '0 auto', display: "flex"}}>Submit</Button>
+
+
+        </header>
+      </div>
 
     </div>
   );
